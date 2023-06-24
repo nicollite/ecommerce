fb5e086352d56aa72a137131f6c45e92eb623766
Nicolas Conte <nickconte70@gmail.com>
2023-06-24 16:30:21 -0300

    add jest setup

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,25 +1,21 @@
-const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const { pathsToModuleNameMapper } = require("ts-jest");
 const { compilerOptions } = require("./tsconfig.json");
 
 const moduleNameMapper = pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/" });
 
 module.exports = {
+  // jest-preset-angular setup
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/src/setup-jest.ts"],
+  globalSetup: "jest-preset-angular/global-setup",
+  // Cache and covarage
   cacheDirectory: "./cache",
   collectCoverage: true,
   collectCoverageFrom: ["src/app/**/*.ts"],
   coverageDirectory: "./coverage",
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.spec.json",
-      isolatedModules: true,
-    },
-  },
-  transform: {
-    "\\.(ts|tsx)$": "<rootDir>/fix-istanbul-decorators.js",
-  },
+  // Paths mapper
   moduleNameMapper,
-  preset: "jest-preset-angular",
-  setupFilesAfterEnv: ["<rootDir>/src/setup-jest.ts"],
+  // Test match and ignore
   testMatch: ["**/?(*.)+(spec|test).ts"],
   testPathIgnorePatterns: [
     "<rootDir>/node_modules/",
