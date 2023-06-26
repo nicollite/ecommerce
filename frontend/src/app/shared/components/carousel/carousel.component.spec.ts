90509a43b8b232017b5c765c378ce484410d21e7
Nicolas Conte <nickconte70@gmail.com>
2023-06-26 19:48:19 -0300

    add tests to carousel compoent

--- frontend/src/app/shared/components/carousel/carousel.component.spec.ts
+++ frontend/src/app/shared/components/carousel/carousel.component.spec.ts
@@ -1,25 +1,78 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { CarouselComponent } from './carousel.component';
+import { CarouselComponent } from "./carousel.component";
+import { SharedModule } from "../../shared.module";
+import { NgbCarouselModule } from "@ng-bootstrap/ng-bootstrap";
+import { getNewCompoenntInstance } from "src/tests/utils/common";
+import { ChangeDetectionStrategy, ChangeDetectorRef } from "@angular/core";
 
-describe('CarouselComponent', () => {
+describe("CarouselComponent", () => {
   let component: CarouselComponent;
   let fixture: ComponentFixture<CarouselComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CarouselComponent ]
-    })
-    .compileComponents();
-  });
+    // Reset mocks between tests
+    jest.resetAllMocks();
 
-  beforeEach(() => {
+    await TestBed.configureTestingModule({
+      declarations: [CarouselComponent],
+      imports: [SharedModule, NgbCarouselModule],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CarouselComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create the component", () => {
+    const detectChangesMock = jest.spyOn(component["cdr"], "detectChanges");
+
+    component.ngAfterViewInit();
+
     expect(component).toBeTruthy();
+    expect(detectChangesMock).toBeCalled();
+  });
+
+  describe("test carousel interactions", () => {
+    it("should test left arrow click", () => {
+      const prevMock = jest.spyOn(component.carousel, "prev").mockReturnValue();
+      const leftArrow = fixture.nativeElement.querySelector('[direction="left"]');
+      leftArrow.click();
+
+      expect(prevMock).toBeCalled();
+    });
+
+    it("should test right arrow click", () => {
+      const nextMock = jest.spyOn(component.carousel, "next").mockReturnValue();
+      const rightArrow = fixture.nativeElement.querySelector('[direction="right"]');
+      rightArrow.click();
+
+      expect(nextMock).toBeCalled();
+    });
+
+    it("should test the clicks on the paginations dots", async () => {
+      component.imgUrls = Array.from({ length: 7 }, (_, i) => i.toString());
+      component.carousel.pause();
+      component.carousel.animation = false;
+      const cdr = fixture.debugElement.injector.get(ChangeDetectorRef);
+      cdr.detectChanges();
+      fixture.detectChanges();
+
+      const selectMock = jest.spyOn(component.carousel, "select");
+      const dots: HTMLElement[] = fixture.nativeElement.querySelectorAll(".dot");
+
+      expect(dots[0].classList.contains("active")).toBeTruthy();
+
+      dots[3].click();
+      fixture.detectChanges();
+      expect(dots[3].classList.contains("active")).toBeTruthy();
+
+      dots[6].click();
+      fixture.detectChanges();
+      expect(dots[6].classList.contains("active")).toBeTruthy();
+
+      expect(selectMock).toHaveBeenNthCalledWith(1, "ngb-slide-3");
+      expect(selectMock).toHaveBeenNthCalledWith(2, "ngb-slide-6");
+    });
   });
 });
