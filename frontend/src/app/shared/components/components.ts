e50e768a08233597889938d3d05ebb61bba5682e
Nicolas Conte <nickconte70@gmail.com>
2023-07-06 15:46:36 -0300

    add components

--- frontend/src/app/shared/components/components.ts
+++ frontend/src/app/shared/components/components.ts
@@ -1,4 +1,11 @@
 import { ArrowNavigatorComponent } from "./arrow-navigator/arrow-navigator.component";
+import { CardListComponent } from "./card-list/card-list.component";
+import { CardComponent } from "./card/card.component";
 import { CarouselComponent } from "./carousel/carousel.component";
 
-export const components = [ArrowNavigatorComponent, CarouselComponent];
+export const components = [
+  ArrowNavigatorComponent,
+  CarouselComponent,
+  CardComponent,
+  CardListComponent,
+];
